e07336d1652eef21e00dec55e2ef6472324cffe7
Ishaan Bedi <bediishaan6@gmail.com>
2024-04-13 13:18:01 +0530

    Init shadcn/ui + Tremor

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -21,9 +22,9 @@ export default async function AuthButton() {
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <Button>
           Logout
-        </button>
+        </Button>
       </form>
     </div>
   ) : (

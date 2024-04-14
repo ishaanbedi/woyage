1bd6867903937dac14bf3f76828223cf09cae9a7
Ishaan Bedi <bediishaan6@gmail.com>
2024-04-14 11:26:10 +0530

    Implement tracking websites

--- app/api/public-script/route.ts
+++ app/api/public-script/route.ts
@@ -1,6 +1,6 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const cookieStore = cookies();
   const supabase = createServerClient(
@@ -17,15 +17,34 @@ export async function POST(request: Request) {
 
   const data = await request.json();
   const { id, path, browser, referrer, os, device, country, website_id } = data;
+  const { data: website } = await supabase
+    .from("site_domains")
+    .select("*")
+    .eq("domain_name", path.split("/")[2]);
+  if (!website || website.length === 0) {
+    return NextResponse.json({ error: "Website not found" }, { status: 404 });
+  }
+  var found = false;
+  for (let i = 0; i < website.length; i++) {
+    if (website[i].website_id === website_id) {
+      found = true;
+      break;
+    }
+  }
+  if (!found) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
   const { error } = await supabase.from("analytics").insert({
     id: id,
-    path: path,
+    path: path.split("/")[3],
     browser: browser,
     referrer: referrer,
     os: os,
     device: device,
     country: country,
-    website_id: (website_id),
+    website_id: website_id,
+    domain: path,
   });
   if (error) {
     console.error("error adding analytics:", error);
--- next.config.js
+++ next.config.js
@@ -2,19 +2,50 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
-}
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+        ],
+      },
+      {
+        source: "/track.js",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

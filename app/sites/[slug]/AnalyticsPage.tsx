6e17a91026f11d43335effb4974136e353a3a33a
Ishaan Bedi <bediishaan6@gmail.com>
2024-04-14 18:36:56 +0530

    Implement filtering based on time

--- app/sites/[slug]/AnalyticsPage.tsx
+++ app/sites/[slug]/AnalyticsPage.tsx
@@ -6,6 +6,14 @@ import BrowsersCard from "./BrowsersCard";
 import CountryCard from "./CountryCard";
 import DeviceCard from "./DeviceCard";
 import OSCard from "./OSCard";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const AnalyticsPage = ({
   user,
   params,
@@ -17,11 +25,49 @@ const AnalyticsPage = ({
   const supabase = createClient();
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<any[]>([]);
+  const [dateRange, setDateRange] = useState("1");
   const fetchRecords = async () => {
+    var timeFilter;
+    switch (dateRange) {
+      case "0":
+        timeFilter = new Date(
+          new Date().setHours(new Date().getHours() - 1),
+        ).toISOString();
+        break;
+      case "1":
+        timeFilter = new Date(
+          new Date().setDate(new Date().getDate() - 1),
+        ).toISOString();
+        break;
+      case "2":
+        timeFilter = new Date(
+          new Date().setDate(new Date().getDate() - 7),
+        ).toISOString();
+        break;
+      case "3":
+        timeFilter = new Date(
+          new Date().setDate(new Date().getDate() - 30),
+        ).toISOString();
+        break;
+      case "4":
+        timeFilter = new Date(
+          new Date().setDate(new Date().getDate() - 90),
+        ).toISOString();
+        break;
+      case "5":
+        timeFilter = new Date(
+          new Date().setDate(new Date().getDate() - 365),
+        ).toISOString();
+        break;
+      case "6":
+        timeFilter = new Date(new Date().setFullYear(1970)).toISOString();
+        break;
+    }
     const { data, error } = await supabase
       .from("analytics")
       .select("*")
-      .eq("id", params.slug);
+      .eq("id", params.slug)
+      .filter("added_time", "gt", timeFilter);
     if (error) {
       console.error(error);
     } else {
@@ -39,7 +85,7 @@ const AnalyticsPage = ({
   useEffect(() => {
     fetchRecords();
     setLoading(false);
-  }, [user, params]);
+  }, [user, params, dateRange]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -48,12 +94,30 @@ const AnalyticsPage = ({
     return <div>No data found</div>;
   }
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      <BrowsersCard data={data} />
-      <CountryCard data={data} />
-      <DeviceCard data={data} />
-      <OSCard data={data} />
-    </div>
+    <section>
+      <div>
+        <Select value={dateRange} onValueChange={setDateRange}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Last 24 hours" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="0">Last hour</SelectItem>
+            <SelectItem value="1">Last 24 hours</SelectItem>
+            <SelectItem value="2">Last 7 days</SelectItem>
+            <SelectItem value="3">Last 30 days</SelectItem>
+            <SelectItem value="4">Last 90 days</SelectItem>
+            <SelectItem value="5">Last 365 days</SelectItem>
+            <SelectItem value="6">All time</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <BrowsersCard data={data} />
+        <CountryCard data={data} />
+        <DeviceCard data={data} />
+        <OSCard data={data} />
+      </div>
+    </section>
   );
 };
 

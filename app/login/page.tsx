b0f8d0eb1448c38dc0c91c2cf90507154ab43e5b
Eashan Bhatia <bhatiaeashan13@gmail.com>
2024-04-18 21:57:19 +0530

    Adding forgot password dialog

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,18 @@ import { SubmitButton } from "./submit-button";
 import { ArrowLeft } from "lucide-react";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button";
 export default async function Login({
   searchParams,
 }: {
@@ -39,6 +50,7 @@ export default async function Login({
   } else {
     return (
       <section className="flex h-[92vh] items-center justify-evenly bg-background">
+
         <div>
           <img src="https://illustrations.popsy.co/white/keynote-presentation.svg" className="w-96 h-96 my-auto" alt="" />
         </div>
@@ -50,57 +62,73 @@ export default async function Login({
             <ArrowLeft size={16} className="mr-2" />
             Back
           </Link>
-            <h1 className="text-3xl font-bold text-center mb-4">Sign In</h1>
-          <div className="   p-5 rounded-lg border bg-card text-card-foreground shadow-sm w-96 mx-auto">
-          <div className="flex flex-col space-y-1.5 pt-6 pb-9">
+          <h1 className="text-3xl font-bold text-center mb-4">Sign In</h1>
+          <div className="   p-5 rounded-lg border bg-card text-card-foreground shadow-sm w-96 mx-auto overflow-hidden">
+            <div className="flex flex-col space-y-1.5 pt-6 pb-9">
               <h3 className="whitespace-nowrap font-semibold leading-none tracking-tight text-2xl __className_335c89 mb-2">Supalytics</h3>
               <p className="text-sm text-muted-foreground">Welcome Back! Enter your login details</p>
             </div>
-          <form className="animate-in flex-1 flex flex-col w-full justify-center text-foreground">
-            <Label className="text-md" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6"
-              name="email"
-              placeholder="you@example.com"
-              required
-            />
-            <Label className="text-md" htmlFor="password">
-              Password
-            </Label>
-            <Input
-              className="rounded-md px-4 py-2 bg-inherit border"
-              type="password"
-              name="password"
-              placeholder="••••••••"
-              required
-            />
-            <div className="text-sm text-muted-foreground flex justify-end mt-2 mb-7">
-              <Link href="/forgotpassword" className="text-center">Forgot Password?</Link>
-            </div>
-            <SubmitButton
-              formAction={signIn}
-              className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
-              pendingText="Signing In..."
-            >
-              Sign In
-            </SubmitButton>
-            <p className="text-center mt-4 text-primary/80 text-sm">
-              Don't have an account?{" "}
-              <Link href="/signup" className="underline underline-offset-4">
-                Sign Up
-              </Link>
-            </p>
-            {searchParams?.message && (
-              <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-                {searchParams.message}
+            <form className="animate-in flex-1 flex flex-col w-full justify-center text-foreground">
+              <Label className="text-md" htmlFor="email">
+                Email
+              </Label>
+              <Input
+                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                name="email"
+                placeholder="you@example.com"
+                required
+              />
+              <Label className="text-md" htmlFor="password">
+                Password
+              </Label>
+              <Input
+                className="rounded-md px-4 py-2 bg-inherit border"
+                type="password"
+                name="password"
+                placeholder="••••••••"
+                required
+              />
+              <div className="text-sm text-muted-foreground flex justify-end mt-2 mb-7">
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant="link">Forgot Password?</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>Password reset</AlertDialogTitle>
+                      <AlertDialogDescription>
+                        Contact us at hey@ishaanbedi.com and we will send you a link to reset your password.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogAction>OK</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+
+              </div>
+              <SubmitButton
+                formAction={signIn}
+                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2"
+                pendingText="Signing In..."
+              >
+                Sign In
+              </SubmitButton>
+              <p className="text-center mt-4 text-primary/80 text-sm">
+                Don't have an account?{" "}
+                <Link href="/signup" className="underline underline-offset-4">
+                  Sign Up
+                </Link>
               </p>
-            )}
-          </form>
+              {searchParams?.message && (
+                <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                  {searchParams.message}
+                </p>
+              )}
+            </form>
           </div>
         </div>
-        
+
       </section>
     );
   }

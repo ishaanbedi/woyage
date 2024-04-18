bbf47b40a872d97e7c0042108080f0bbb3e38559
Ishaan Bedi <bediishaan6@gmail.com>
2024-04-18 16:56:49 +0530

    Add back button in logout page

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { createClient } from "@/utils/supabase/server";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 export default async function AuthButton() {
@@ -20,11 +21,12 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4 h-[95vh]">
+      <BackButton />
       <form
         action={signOut}
         className="flex flex-col justify-center items-center mx-auto gap-4"
       >
-        <Label>Are you sure you want to logout?</Label>
+        <h1 className="text-2xl font-bold">Are you sure you want to logout?</h1>
         <Button>Logout</Button>
       </form>
     </div>
@@ -32,3 +34,10 @@ export default async function AuthButton() {
     redirect("/")
   );
 }
+
+const BackButton = () => (
+  <Link href="/" className="absolute left-8 top-8 flex items-center gap-2">
+    <ArrowLeftIcon className="w-5 h-5" />
+    <Label>Home</Label>
+  </Link>
+);

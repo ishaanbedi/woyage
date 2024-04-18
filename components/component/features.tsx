ce2cf105cbc9e8fb55df53f33b57bc0bbd11c2f8
Ishaan Bedi <bediishaan6@gmail.com>
2024-04-18 16:51:42 +0530

    Update features.tsx

--- components/component/features.tsx
+++ components/component/features.tsx
@@ -1,101 +1,89 @@
-import { StopwatchIcon } from "@radix-ui/react-icons"
-import { BarChart, BarChart2, Eye, FilterIcon, FlagIcon, HopIcon, LanguagesIcon, LocateFixedIcon, LocateIcon, MoveIcon, PersonStandingIcon, PinIcon, TrafficCone, TrendingUp, UsersRound, View } from "lucide-react"
+import { StopwatchIcon } from "@radix-ui/react-icons";
+import {
+  BarChart2,
+  Eye,
+  FilterIcon,
+  LanguagesIcon,
+  MonitorSmartphoneIcon,
+  PinIcon,
+  TrafficCone,
+  TrendingUp,
+  UsersRound,
+} from "lucide-react";
+import { Card } from "../ui/card";
 export function Features() {
+  const features: [
+    {
+      title: string;
+      description: string;
+      icon: React.ReactNode;
+    },
+  ] = [
+    {
+      title: "Visitors",
+      description:
+        "Track and analyze of your visitors and their behavior, in real-time.",
+      icon: <UsersRound className="w-12 h-12" />,
+    },
+    {
+      title: "Location",
+      description:
+        "See where your visitors are coming from, from all around the world.",
+      icon: <PinIcon className="w-12 h-12" />,
+    },
+    {
+      title: "Languages",
+      description:
+        "Analyze the languages your visitors speak, and tailor your content.",
+      icon: <LanguagesIcon className="w-12 h-12" />,
+    },
+    {
+      title: "Real-Time Data",
+      description: "See what's happening in real-time, as it happens.",
+      icon: <StopwatchIcon className="w-12 h-12" />,
+    },
+    {
+      title: "Traffic Sources",
+      description:
+        "Analyze referral, direct, and search traffic sources to your website.",
+      icon: <TrafficCone className="w-12 h-12" />,
+    },
+    {
+      title: "Device Analytics",
+      description:
+        "Analyze visitor's devices, browsers, and operating systems.",
+      icon: <MonitorSmartphoneIcon className="w-12 h-12" />,
+    },
+  ];
   return (
     <section className="w-full py-6">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-2 text-center">
-          <div className="space-y-2">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">Platform Features</h1>
-            <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed dark:text-gray-400">
-            An overview of all the core features Supalytics provides.
+          <div className="space-y-2 flex flex-col justify-center items-center">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center">
+              {process.env.NEXT_PUBLIC_SITE_NAME} is packed with features
+            </h1>
+            <p className="max-w-[700px] text-gray-500 md:text-xl/relaxed dark:text-gray-400 text-center">
+              An overview of all the core features{" "}
+              {process.env.NEXT_PUBLIC_SITE_NAME} provides.
             </p>
           </div>
         </div>
-        <div className="mx-auto grid max-w-3xl items-start gap-6 min-[600px]:grid-cols-3 py-12 md:gap-10 md:max-w-5xl lg:grid-cols-3 lg:gap-8 lg:max-w-6xl">
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <UsersRound className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Visitor Info</h3>
-            <p className="flex justify-center text-center items-center text-sm text-gray-500 dark:text-gray-400">
-            Get detailed information about your visitors like their device, browser, OS and location.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <Eye className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Page views</h3>
-            <p className="flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            Knowing which of your pages gets the most traffic is essential to improving your website content.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <TrafficCone className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Traffic sources</h3>
-            <p className=" flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            See where your traffic is coming from to better understand where you should be spending your effort.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <FilterIcon className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Filtering</h3>
-            <p className="flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            Gain further insight into your data by applying filters like country, browser, and URL.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <PinIcon className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Location</h3>
-            <p className="flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            Find out where your visitors are coming from including the city, region and country.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <LanguagesIcon className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Languages</h3>
-            <p className="felx justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            Know which languages are the most popular among your visitors to help you tailor your content.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900dark:border-gray-800">
-              <StopwatchIcon className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Realtime Data</h3>
-            <p className="flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            Data available in seconds, not days. The data that Supalytics collects is immediately available on your dashboard
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900dark:border-gray-800">
-              <BarChart2 className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Reports</h3>
-            <p className="flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            Supalytics comes with out of the box reporting that enables you to gain insights from all your website data.
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-2">
-            <div className="w-12 h-12 rounded-lg border border-gray-900 dark:border-gray-800">
-              <TrendingUp className="w-12 h-12" />
-            </div>
-            <h3 className="font-bold">Bounce rate</h3>
-            <p className="flex justify-center items-center text-center text-sm text-gray-500 dark:text-gray-400">
-            See which pages keep your visitors engaged versus those they are abandoning.
-            </p>
-          </div>
+        <div className="mx-auto grid max-w-3xl items-start gap-6 min-[600px]:grid-cols-3 py-12 md:gap-10 md:max-w-5xl lg:grid-cols-3 grid-cols-2 lg:gap-8 lg:max-w-6xl">
+          {features.map((feature, index) => (
+            <Card
+              key={index}
+              className="flex flex-col items-center justify-center space-y-2 p-3 min-h-64 shadow-none hover:border hover:shadow"
+            >
+              <div className="flex items-center justify-center w-16 h-16 p-4 text-white bg-primary rounded-full">
+                {feature.icon}
+              </div>
+              <h2 className="text-xl font-bold text-center">{feature.title}</h2>
+              <p className="text-gray-500 text-center">{feature.description}</p>
+            </Card>
+          ))}
         </div>
       </div>
     </section>
-  )
-};
+  );
+}

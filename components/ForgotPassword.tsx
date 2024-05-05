1cd259d861eb4a96f8fc9517c7c0eccf534d3467
Ishaan Bedi <bediishaan6@gmail.com>
2024-05-05 15:16:58 +0530

    chore: Add email reset functionality to ForgotPassword component

--- components/ForgotPassword.tsx
+++ components/ForgotPassword.tsx
@@ -9,9 +9,28 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import Link from "next/link";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { createClient } from "@/utils/supabase/client";
+import { toast } from "sonner";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const sendResetLink = async () => {
+    setLoading(true);
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: `${window.location.origin}/reset-password`
+    })
+    if (error) {
+      toast.error(error.message);
+    }
+    if (data) {
+      toast.success("Reset link sent to your email");
+    }
+    setLoading(false);
+  }
   return (
     <div>
       <Dialog>
@@ -21,18 +40,12 @@ const ForgotPassword = () => {
             <DialogTitle>Forgot Password?</DialogTitle>
             <DialogDescription>No worries, we got you!</DialogDescription>
           </DialogHeader>
-          <div className=" text-center">
-            Just email us at{" "}
-            <Link
-              href={`mailto:${process.env.NEXT_PUBLIC_SUPPORT_EMAIL}`}
-              className="underline"
-            >
-              {process.env.NEXT_PUBLIC_SUPPORT_EMAIL}
-            </Link>{" "}
-            and we'll help you reset your password.
-            {/* <Label htmlFor="email">Email</Label>
-            <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-            <Button>Send Reset Link</Button> */}
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Input value={email} onChange={(e) => setEmail(e.target.value)} disabled={loading} />
+            <Button onClick={sendResetLink} className="mt-3" disabled={loading}>
+              {loading ? "Sending..." : "Send Reset Link"}
+            </Button>
           </div>
         </DialogContent>
       </Dialog>
